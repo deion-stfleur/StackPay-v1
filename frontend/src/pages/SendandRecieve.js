488967b99112e98984d509f28f3759654af174e2
deion-stfleur <deionstfleur13@gmail.com>
2024-05-05 18:26:08 -0400

    first
    test

--- frontend/src/pages/SendandRecieve.js
+++ frontend/src/pages/SendandRecieve.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../supabase';
+import { useNavigate } from 'react-router-dom';
+import "../App.css"
 
 function SendandRecieve() {
   return (
-    <div>SendandRecieve</div>
+    <div>
+        <div className='main-panel-container'>
+        <div className="left-panel">
+
+<div className="filter">
+       
+       <label>Activity</label>
+   </div>
+ 
+    <div className="filter">
+       
+        <label>Money</label>
+    </div>
+
+    <div className="filter">
+       
+        <label>Send/Recieve</label>
+    </div>
+
+    <div className="filter">
+        <label>Account</label>
+    </div>
+
+
+
+
+    <div className="filter">
+       <div onClick={handleSignOut}>
+        <label>Log Out</label>
+       </div>
+    </div>
+  
+  
+   
+
+   
+</div>
+         
+
+        </div>
+    </div>
   )
 }
 
